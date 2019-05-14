f777a61e3a22e3bfec04d66e79aed883ad7935c2
Ivan Mironov <ivail89@mail.ru>
2019-05-14 22:30:24 +0300

    Parse data

--- LZW/parse.cpp
+++ LZW/parse.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 int main()
 {
   cout << "Start!" << endl;
-  ifstream file ("AirQualityUCI.csv");
+  ifstream file ("AirQualityUCIsed.csv");
   if (!file){
     cout << "File with problem" << endl;
   } else {
     cout << "All right!" << endl;
   }
 
-  string date, time, header;
+  string date, time, header, cell;
   float co, c6h6, t, rh, ah;
   int pts1, nmhc,pts2, no, pts3, no2, pts4, o3;
 
   getline(file, header);
-  file >> date >> time >> co >> pts1 >> nmhc >> c6h6 >> pts2 >> no >>
-    pts3 >> no2 >> pts4 >> o3 >> t >> rh >> ah;
+  
+  for (int i=0; i<3; i++){
+  // Read all rows 
+    getline(file, date, ';');
+    getline(file, time, ';');
 
-  cout << header << endl;
-  cout << date << ah << endl;
+    getline(file, cell, ';'); co = atof(cell.c_str());
+    getline(file, cell, ';'); pts1 = atof(cell.c_str());
+    getline(file, cell, ';'); nmhc = atof(cell.c_str());
+    getline(file, cell, ';'); c6h6 = atof(cell.c_str());
+    getline(file, cell, ';'); pts2 = atof(cell.c_str());
+    getline(file, cell, ';'); no = atof(cell.c_str());
+    getline(file, cell, ';'); pts3 = atof(cell.c_str());
+    getline(file, cell, ';'); no2 = atof(cell.c_str());
+    getline(file, cell, ';'); pts4 = atof(cell.c_str());
+    getline(file, cell, ';'); o3 = atof(cell.c_str());
+    getline(file, cell, ';'); t = atof(cell.c_str());
+    getline(file, cell, ';'); rh = atof(cell.c_str());
+    getline(file, cell, ';'); ah = atof(cell.c_str());
+
+    getline(file, cell); // The tail of the string
+  }
+
   return 0;
 }
